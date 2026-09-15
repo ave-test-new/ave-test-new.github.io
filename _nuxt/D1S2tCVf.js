8b25ae230b96504cfebd512ad3717b0e878a3d9c
amy8023 <2847361548@qq.com>
2026-09-15 18:35:05 +0800

    deploy

---
+++ _nuxt/D1S2tCVf.js
@@ -0,0 +1,1 @@
+import{Yt as e,ss as i}from"./qG8b3oNA.js";var t=i("favToken",()=>{const i=e({prefix:"favToken",defaultFixedWidth:300});return{...i,visible:i.visible,favTokenBoundingRect:i.boundingRect}});export{t};
